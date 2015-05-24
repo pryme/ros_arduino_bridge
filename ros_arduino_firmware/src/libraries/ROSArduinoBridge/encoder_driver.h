699b49226ba4b21c9aa6584fd9d3cbd594d7db7c
Patrick <pryme4gh@gmail.com>
2015-05-23 17:28:21 -0700

    Added option to directly read encoders with Teensy
    Adds #elif defined block for my specific Teensy / encoder hardware

--- ros_arduino_firmware/src/libraries/ROSArduinoBridge/encoder_driver.h
+++ ros_arduino_firmware/src/libraries/ROSArduinoBridge/encoder_driver.h
@@ -1,7 +1,12 @@
 /* *************************************************************
    Encoder driver function definitions - by James Nugen
+   
+   Modified by PR:  2015-5-9
    ************************************************************ */
    
+
+#ifndef ENCODER_DRIVER_H
+#define ENCODER_DRIVER_H
    
 #ifdef ARDUINO_ENC_COUNTER
   //below can be changed, but should be PORTD pins; 
@@ -12,9 +17,19 @@
   //below can be changed, but should be PORTC pins
   #define RIGHT_ENC_PIN_A PC4  //pin A4
   #define RIGHT_ENC_PIN_B PC5   //pin A5
+#elif defined PR_TEENSY_ENC_COUNTER
+  // define the pins for reading encoder
+  #define LEFT_ENC_PIN PIN_D0  // will use INT0
+  #define RIGHT_ENC_PIN PIN_D1  // will use INT1
+#else
+  #error An encoder driver must be selected!
 #endif
    
 long readEncoder(int i);
 void resetEncoder(int i);
 void resetEncoders();
+unsigned long readEncoderInterval(int i);
+boolean isFreshEncoderInterval(int i);
+void initEncoders();
 
+#endif
--- ros_arduino_firmware/src/libraries/ROSArduinoBridge/encoder_driver.ino
+++ ros_arduino_firmware/src/libraries/ROSArduinoBridge/encoder_driver.ino
@@ -5,6 +5,8 @@
    a particular encoder board or library. Then add the appropriate
    #define near the top of the main ROSArduinoBridge.ino file.
    
+   Modified by PR:  2015-5-9
+
    ************************************************************ */
    
 #ifdef USE_BASE
@@ -68,6 +70,95 @@
       return;
     }
   }
+
+#elif defined PR_TEENSY_ENC_COUNTER
+volatile boolean EL_isFresh = true;  // flag fresh interval value from left enc interrupt
+volatile boolean ER_isFresh = true;    
+volatile unsigned long EL_last_interval = 0;  // most recent interval (us), left encoder
+volatile unsigned long ER_last_interval = 0;  
+volatile boolean EL_keep = false; // flag to save only every other micros count
+volatile boolean ER_keep = false;  
+elapsedMicros EL_micros = 0;  // counts micros for left encoder
+elapsedMicros ER_micros =0;  
+volatile long EL_ticks = 0L;  // accumulates tick count for left encoder
+volatile long ER_ticks = 0L;
+// TODO:  check whether any of the above get used out of this scope.
+
+ISR(INT0_vect){  // interrupt routine for left encoder
+	if (EL_keep){  // only save interval every other tick to eliminate comparator asymmetry
+		EL_last_interval = EL_micros;  // save the interval (us)
+		EL_micros = 0;  // reset the elapssedMicros object
+		EL_isFresh = true;  // set fresh data flag
+	}
+	EL_keep = !EL_keep;  // toggle 
+	EL_ticks += 1L;  // increment the tick count every tick
+	// TODO:  there might be a faster way using bit shifting
+}
+ISR(INT1_vect){  // interrupt routine for right encoder
+	if (ER_keep){  // only save interval every other tick to eliminate comparator asymmetry
+		ER_last_interval = ER_micros;  // save the interval (us)
+		ER_micros = 0;  // reset the elapssedMicros object
+		ER_isFresh = true;  // set fresh data flag
+	}
+	ER_keep = !ER_keep;  // toggle 
+	ER_ticks += 1L;  // increment the tick count every tick
+}
+
+/* Wrap the encoder reading function */
+long readEncoder(int i) {
+	if (i == LEFT) {
+		return EL_ticks;
+	} else {
+		return ER_ticks;
+	}
+}
+
+/* Wrap the encoder reset function */
+void resetEncoder(int i) {
+	if (i == LEFT){
+		EL_ticks = 0L;
+		return;
+	} else { 
+		ER_ticks = 0L;
+		return;
+	}
+}
+
+/* Return the most recent encoder interval */
+unsigned long readEncoderInterval(int i){
+	if (i == LEFT){
+        EL_isFresh = false;
+		return EL_last_interval;
+	} else { 
+        ER_isFresh = false;
+		return ER_last_interval;
+	}
+}  // end readEncoderInterval
+
+/* Return flag indicating if interval data is fresh */
+boolean isFreshEncoderInterval(int i){
+	if (i == LEFT){
+		return EL_isFresh;
+	} else { 
+		return ER_isFresh;
+	}
+}  // end isFreshEncoderInterval
+
+/* Initialize the Teensy pin modes and interrupts for PR encoders */
+void initEncoders(){
+	pinMode(LEFT_ENC_PIN , INPUT);
+	pinMode(RIGHT_ENC_PIN , INPUT);  
+	// enable interrupts
+	EIMSK |= (1<<0);  // set bit 0 of EIMSK to enable INT0
+	EIMSK |= (1<<1);  // set bit 1 of EIMSK to enable INT1
+	EICRA |= (1<<0);  // set bit 0 of EICRA - this and following line define edge trigger
+	EICRA &= ~(1<<1);  // clear bit 1 of EICRA - edge trigger for INT0
+	EICRA |= (1<<2);  // set bit 0 of EICRA - this and following line define edge trigger
+	EICRA &= ~(1<<3);  // clear bit 1 of EICRA - edge trigger for INT1
+	sei();  // global interrupt enable
+    resetEncoders();	
+}  // end initEncoders
+
 #else
   #error A encoder driver must be selected!
 #endif
